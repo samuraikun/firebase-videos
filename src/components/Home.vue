c05946c7eb630ca672858ceb01341dd18fbdcf0e
samuraikun <tazitaziawawa@gmail.com>
2018-09-01 20:40:57 +0900

    cosmetic

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Welcome to {{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }  
+}
+</script>
+
+<style scoped>
+
+</style>
